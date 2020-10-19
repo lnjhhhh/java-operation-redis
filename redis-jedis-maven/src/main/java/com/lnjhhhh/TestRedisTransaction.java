b3c0b965b83a842dea38d32519a01368ec3a735e
lnjhhhh <lnjhhhh@163.com>
2020-10-19 21:12:08 +0800

    基于Jedis操作Redis的相关练习

--- redis-jedis-maven/src/main/java/com/lnjhhhh/TestRedisTransaction.java
+++ redis-jedis-maven/src/main/java/com/lnjhhhh/TestRedisTransaction.java
@@ -1,7 +1,10 @@
 package com.lnjhhhh;
 
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Response;
 import redis.clients.jedis.Transaction;
+
+import java.util.List;
 
 /**
  * @Author lnjhhhh
@@ -14,7 +17,15 @@ public class TestRedisTransaction {
     public static void main(String[] args) {
         Jedis jedis = new Jedis("192.168.56.100", 6379);
         Transaction multi = jedis.multi();
-
+        Response<String> set = multi.set("name", "zhangsan");
+        System.out.println(set);
+        multi.set("age", "20");
+        multi.set("gender", "man");
+        //执行事物
+        List<Object> exec = multi.exec();
+        System.out.println(exec);
+        multi.close();
+        jedis.close();
     }
 
 }
