43599b92babe74a9d616cbe5c639f7acaa8d55d8
lnjhhhh <lnjhhhh@163.com>
2020-10-20 09:44:28 +0800

    基于SpringBoot整合Redis的练习

---
+++ redis-spring-boot/src/test/java/com/lnjhhhh/RedisSpringBootApplicationTests.java
@@ -0,0 +1,14 @@
+package com.lnjhhhh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RedisSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("aaa");
+    }
+
+}
