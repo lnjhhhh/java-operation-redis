7c5a2f8fd7a43d40f46bd12bc4e91b2412ee7bf5
lnjhhhh <lnjhhhh@163.com>
2020-10-20 20:52:59 +0800

    基于SpringBoot整合Redis的练习

---
+++ redis-spring-boot/src/main/java/com/lnjhhhh/config/RedisConfig.java
@@ -0,0 +1,50 @@
+package com.lnjhhhh.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.PolymorphicTypeValidator;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @Author lnjhhhh
+ * @CreateDate 2020/10/20 20:35
+ * @Version 1.0
+ */
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        //下面两句选其一即可
+        //om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        om.activateDefaultTyping(LaissezFaireSubTypeValidator.instance,
+                ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        // key采用String的序列化方式
+        template.setKeySerializer(stringRedisSerializer);
+        // hash的key也采用String的序列化方式
+        template.setHashKeySerializer(stringRedisSerializer);
+        // value序列化方式采用jackson
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+        // hash的value序列化方式采用jackson
+        template.setHashValueSerializer(jackson2JsonRedisSerializer);
+        template.afterPropertiesSet();
+        return template;
+    }
+
+}
--- redis-spring-boot/src/test/java/com/lnjhhhh/RedisSpringBootApplicationTests.java
+++ redis-spring-boot/src/test/java/com/lnjhhhh/RedisSpringBootApplicationTests.java
@@ -1,14 +1,57 @@
 package com.lnjhhhh;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.*;
+
+import java.util.List;
 
 @SpringBootTest
 public class RedisSpringBootApplicationTests {
 
+    @Autowired
+    private RedisTemplate<Object, Object> redisTemplate;
+
+
     @Test
     void contextLoads() {
-        System.out.println("aaa");
+        BoundValueOperations<Object, Object> name = redisTemplate.boundValueOps("name");
+        BoundStreamOperations<Object, Object, Object> age = redisTemplate.boundStreamOps("age");
+        redisTemplate.multi();
+        redisTemplate.exec();
+        ValueOperations<Object, Object> valueOperations = redisTemplate.opsForValue();
+    }
+
+    @Test
+    public void testBoundXxxOps() {
+        //获取一个绑定数据类型为String，并且key为name的操作对象
+        BoundValueOperations<Object, Object> name = redisTemplate.boundValueOps("name");
+        //因为上面已经设置了key为name，所以这里只需要传递值即可
+        name.set("zhangsan");
+        /*
+            boundValueOps(T key)：获取一个操作String类型的绑定key对象
+            boundGeoOps(T key)：获取一个操作Geo类型的绑定key对象
+            boundHashOps(T key)：获取一个操作Hash类型的绑定key对象
+            boundListOps(T key)：获取一个操作List类型的绑定key对象
+            boundSetOps(T key)：获取一个操作Set类型的绑定key对象
+            boundZSetOps(T key)：获取一个操作ZSet类型的绑定Key对象
+         */
+    }
+
+    @Test
+    public void testXxxOps() {
+        ValueOperations<Object, Object> value = redisTemplate.opsForValue();
+        value.set("name", "lisi");
+        /*
+            opsForValue()：获取一个操作String类型的对象
+            opsForGeo()：获取一个操作Geo类型的对象
+            opsForHash()：获取一个操作Hash类型的对象
+            opsForList()：获取一个操作List类型的对象
+            opsForSet()：获取一个操作Set类型的对象
+            opsForZSet()：获取一个操作ZSet类型的对象
+            opsForHyperLogLog()：获取一个操作HyperLogLog类型的对象
+         */
     }
 
 }
